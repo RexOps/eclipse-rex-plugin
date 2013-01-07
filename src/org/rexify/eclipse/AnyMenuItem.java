d51719849a4fca69d5dde44272024187ff43cea4
Jan <krimdomu@googlemail.com>
2013-01-07 02:52:06 +0100

    experiments with windows

--- src/org/rexify/eclipse/AnyMenuItem.java
+++ src/org/rexify/eclipse/AnyMenuItem.java
@@ -4,6 +4,8 @@ package org.rexify.eclipse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.swt.SWT;
@@ -33,18 +35,32 @@ public class AnyMenuItem extends ContributionItem {
 				create_rexfile.addSelectionListener(AnyMenu.getCreateRexfileAction());
 			}
 			else {
-				// populating menu from rex tasks
-				Runtime run = Runtime.getRuntime();
+				List<String> cmd = new ArrayList<String>();
+				
+				if(System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+					cmd.add("cmd");
+					cmd.add("/C");
+					cmd.add("rex.bat -mTq");
+				}
+				else {
+					cmd.add("/bin/sh");
+					cmd.add("-c");
+					cmd.add("rex -mTq");
+				}
+				
 				try {
-					// if rex is installed via cpan this is not needed.
-					String[] s = {"PERL5LIB=/Users/jan/Projekte/rex/lib", "PATH=/Users/jan/Projekte/rex/bin:/Users/jan/perl5/perlbrew/perls/perl-5.14.2/bin:/usr/bin:/usr/local/bin:/bin:/opt/local"};
-					//String[] s = {};
+					ProcessBuilder builder = new ProcessBuilder(cmd);
+					builder.directory(AnyMenu.getProjectPath().toFile());
+					Process process = builder.start();
 					
-					Process p = run.exec("rex -mTq", s, AnyMenu.getProjectPath().toFile());
-					p.waitFor();
-					BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					BufferedReader buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
+					BufferedReader err_buf = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+					
 					String line;
+					
 					while( (line = buf.readLine()) != null) {
+						System.out.println(line);
+						
 						// split lines
 						String[] splitted_line = line.split(" = ");
 						String task_name = splitted_line[0].substring(1, splitted_line[0].length()-1);
@@ -53,13 +69,75 @@ public class AnyMenuItem extends ContributionItem {
 						task_item.setText("Run Task: " + task_name);
 						task_item.addSelectionListener(AnyMenu.getRunTaskAction(task_name));
 					}
+					
+					buf.close();
+
+					while( (line = err_buf.readLine()) != null) {
+						System.out.println("ERR: " + line);
+					}
+
+					err_buf.close();
+
+					int result = process.waitFor();
+					System.out.println("Result: " + result);
+
+				} catch(IOException io_ex) {
+					System.out.println(io_ex);
+				} catch(InterruptedException int_ex) {
+					// Display error message
+					System.out.println(int_ex.toString());
+				}
+
+
+				// populating menu from rex tasks
+				/*Runtime run = Runtime.getRuntime();
+				try {
+					// if rex is installed via cpan this is not needed.
+					//String[] s = {"PERL5LIB=/Users/jan/Projekte/rex/lib", "PATH=/Users/jan/Projekte/rex/bin:/Users/jan/perl5/perlbrew/perls/perl-5.14.2/bin:/usr/bin:/usr/local/bin:/bin:/opt/local"};
+					//String[] s = {"PATH=C:\\strawberry\\perl\\bin;C:\\strawberry\\perl\\site\\bin"};
+					String[] s = {};
+					String[] cmd = {"C:\\strawberry\\perl\\bin\\perl.exe", "c:/test.pl"};
+					
+					Process p = run.exec(cmd); //, s, AnyMenu.getProjectPath().toFile());
+					
+					BufferedReader buf = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					BufferedReader err_buf = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+					
+					String line;
+					
+					while( (line = buf.readLine()) != null) {
+						System.out.println(line);
+					}
+					
+					buf.close();
+
+					while( (line = err_buf.readLine()) != null) {
+						System.out.println("ERR: " + line);
+					}
+
+					err_buf.close();
+					
+					int result = p.waitFor();
+					System.out.println("Result: " + result);
+
+					
+					/*
+						// split lines
+						String[] splitted_line = line.split(" = ");
+						String task_name = splitted_line[0].substring(1, splitted_line[0].length()-1);
+						
+						MenuItem task_item = new MenuItem(menu, SWT.NORMAL, index);
+						task_item.setText("Run Task: " + task_name);
+						task_item.addSelectionListener(AnyMenu.getRunTaskAction(task_name));
+					}
+	*** /				
 				} catch(IOException ex) {
 					// Display error message
 					System.out.println(ex.toString());
 				} catch(InterruptedException int_ex) {
 					// Display error message
 					System.out.println(int_ex.toString());
-				}
+				} */
 			}
 		}
 	}
